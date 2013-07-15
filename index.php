88e5acec307c441fa068a0f665b0f1782ee211fc
ivnaquino <ivn.univ@gmail.com>
2013-07-15 15:06:29 -0500

    ActiveRecord

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	define("DS", DIRECTORY_SEPARATOR);
 	define("ROOT", realpath(dirname(__FILE__)).DS);
 	define("APP_PATH", ROOT.'application'.DS);
+	define("LIBS_PATH", ROOT.'libs'.DS);
 
 	require_once APP_PATH."Config.php";
 	require_once APP_PATH."Request.php";
@@ -10,12 +11,18 @@
 	require_once APP_PATH."Model.php";
 	require_once APP_PATH."Registro.php";
 	require_once APP_PATH."View.php";
+	require_once LIBS_PATH."activerecord".DS."ActiveRecord.php";
 
 	try{
+		ActiveRecord\Config::initialize(function($cfg){
+			    $cfg->set_model_directory('models');
+			    $cfg->set_connections(array(
+			        'development' => 'mysql://'.DB_USER.':'.DB_PASS.'@'.DB_HOST.'/'.DB_NAME.''));
+		});
 		Bootstrap::run(new Request);
 	}catch(Exception $e){
-		//echo $e->getMessage();
-		header("Location: ".BASE_URL."error/error404");
+		echo $e->getMessage();
+		//header("Location: ".BASE_URL."error/error404");
 	}
 
 ?>
